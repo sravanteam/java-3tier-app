6ace8999cd755b8c673a1365c6955de4c03ec740
sravanteam <sravandevops949494@gmail.com>
2026-05-01 01:13:12 +0530

    Update variables.tf

--- infrastructure/modules/rds/variables.tf
+++ infrastructure/modules/rds/variables.tf
@@ -37,17 +37,17 @@ variable "db_password" {
   sensitive   = true
 }
 
-output "rds_instance_id" {
-  description = "RDS instance ID"
-  value       = aws_db_instance.main.id
-}
+#output "rds_instance_id" {
+ # description = "RDS instance ID"
+  #value       = aws_db_instance.main.id
+#}
 
-output "rds_instance_endpoint" {
-  description = "RDS instance endpoint"
-  value       = aws_db_instance.main.endpoint
-}
+#output "rds_instance_endpoint" {
+ # description = "RDS instance endpoint"
+  #value       = aws_db_instance.main.endpoint
+#}
 
-output "rds_instance_port" {
-  description = "RDS instance port"
-  value       = aws_db_instance.main.port
-}
+#output "rds_instance_port" {
+ # description = "RDS instance port"
+  #value       = aws_db_instance.main.port
+#}
