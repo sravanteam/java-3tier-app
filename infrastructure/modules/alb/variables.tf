0244ca90e39befb4bd22e4dccbf949ee9a9add2e
sravanteam <sravandevops949494@gmail.com>
2026-05-01 01:06:53 +0530

    Update variables.tf

--- infrastructure/modules/alb/variables.tf
+++ infrastructure/modules/alb/variables.tf
@@ -15,17 +15,17 @@ variable "public_subnets" {
   type        = list(string)
 }
 
-output "alb_arn" {
-  description = "ALB ARN"
-  value       = aws_lb.main.arn
-}
+#output "alb_arn" {
+ # description = "ALB ARN"
+  #value       = aws_lb.main.arn
+#}
 
-output "alb_dns_name" {
-  description = "ALB DNS name"
-  value       = aws_lb.main.dns_name
-}
+#output "alb_dns_name" {
+ # description = "ALB DNS name"
+  #value       = aws_lb.main.dns_name
+#}
 
-output "target_group_arn" {
-  description = "Target group ARN"
-  value       = aws_lb_target_group.main.arn
-}
+#output "target_group_arn" {
+ # description = "Target group ARN"
+  #value       = aws_lb_target_group.main.arn
+#}
