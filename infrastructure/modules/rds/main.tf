f640f132e88a934dcaf8ba63034bbc1092c105bf
sravanteam <sravandevops949494@gmail.com>
2026-05-01 03:08:51 +0530

    Update main.tf

--- infrastructure/modules/rds/main.tf
+++ infrastructure/modules/rds/main.tf
@@ -29,7 +29,7 @@ resource "aws_db_instance" "main" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = var.security_group_ids
 
-  backup_retention_period = 7
+  backup_retention_period = 0
   backup_window          = "03:00-04:00"
   maintenance_window     = "sun:04:00-sun:05:00"
 
